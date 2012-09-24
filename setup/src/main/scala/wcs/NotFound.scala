abf0821aa44e6b3dc455be7bc6aaac551c1171f2
Michele Sciabarra <michele@sciabarra.com>
2012-09-24 19:35:55 +0200

    backup commit - changed a lot of files but nothing work here

--- setup/src/main/scala/wcs/NotFound.scala
+++
@@ -1,9 +0,0 @@
-package wcs
-
-import scala.xml.NodeSeq
-
-class ElementNotFound extends Element {
-
-  def apply(ics: ICS): NodeSeq = <h1>Not Found</h1>
-
-}
--- src/main/scala/app/Template/Typeless/swLayout.scala
+++
@@ -1,7 +0,0 @@
-package app.Template.Typeless
-
-import wcs._
-
-class swLayout extends Element {
-  def apply(ics: ICS) = <p>Layout</p>
-}
