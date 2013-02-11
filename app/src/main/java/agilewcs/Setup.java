2ab305c1c32ec3fc07e26c1bfe01b027bfbfe2b7
Michele Sciabarra <michele@sciabarra.com>
2013-02-11 10:53:18 +0100

    Static asset added, closes #12

--- app/src/main/java/agilewcs/Setup.java
+++ app/src/main/java/agilewcs/Setup.java
@@ -50,6 +50,7 @@ public class Setup extends wcs.java.Setup {
 						.cache("false", "false") //
 						.description("Menu Top (Java)"), //
 
+						/*
 				new Template("Agile_Article", "AwLayout", Template.LAYOUT,
 						agilewcs.article.Layout.class)//
 						.cache("false", "false") //
@@ -59,7 +60,8 @@ public class Setup extends wcs.java.Setup {
 						agilewcs.article.Summary.class)//
 						.cache("false", "false") //
 						.description("Article Summary (Java)"), //
-
+*/
+						
 				// test runner
 				new CSElement("JUnitRunner", agilewcs.tests.JUnitRunner.class), //
 				new SiteEntry("JUnitRunner", false) };
---
+++ export/envision/AgileWCS,ScalaWCS-11g/src/jsp/cs_deployed/AwMenuBottom.jsp
@@ -0,0 +1,9 @@
+<%@ taglib prefix="cs" uri="futuretense_cs/ftcs1_0.tld"
+%><%@ taglib prefix="asset" uri="futuretense_cs/asset.tld"
+%><%@ taglib prefix="ics" uri="futuretense_cs/ics.tld"
+%><%@ taglib prefix="render" uri="futuretense_cs/render.tld"
+%><%@ page import="wcs.core.WCS"
+%><cs:ftcs><ics:if condition='<%=ics.GetVar("tid")!=null%>'><ics:then><render:logdep
+cid='<%=ics.GetVar("tid")%>' c="Template"/></ics:then></ics:if><%
+String r = WCS.dispatch(ics, "agilewcs.page.MenuBottom");
+if(r!=null) ics.StreamText(r); %></cs:ftcs>
