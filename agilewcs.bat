8bc0a803b6d1c079741e5b11d2e85afafd82e1b2
Michele Sciabarra <michele@sciabarra.com>
2013-03-06 17:36:31 +0100

    modified agilewcs.bat

--- agilewcs.bat
+++ agilewcs.bat
@@ -1,9 +1,10 @@
 @echo off
 if exist build.sbt goto run
-echo "To start, copy build.sbt.dist in build.sbt and EDIT IT. Reading the README.md does not hurt, too."
+echo "To start, copy build.sbt.sbt to build.sbt and EDIT IT."
+echo "Reading the README.md and the documentation does not hurt, too." 
 goto end 
 :run
 set SCRIPT_DIR=%~dp0
-java -Xms256M -Xmx1024M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384M -jar "%SCRIPT_DIR%bin\sbt-launch.jar" %*
+java -Xms256M -Xmx1024M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384M -Dsbt.boot.directory=project\boot -jar "%SCRIPT_DIR%bin\sbt-launch.jar" %*
 :end
 
