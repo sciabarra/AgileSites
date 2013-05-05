82ded8ccc37849e76ca5c9a9f11d948c6b2ae535
Michele Sciabarra <michele@sciabarra.com>
2013-05-05 15:26:12 +0200

    warnings for javac and paths [fix #3961270]

---
+++ agilesites.cmd
@@ -0,0 +1,28 @@
+@echo off
+if not defined JAVA_HOME goto nojavahome
+set PATH="%JAVA_HOME%"\bin;%PATH%
+:nojavahome
+java -version
+if errorlevel 9009 if not errorlevel 9010 goto notfoundjava
+javac -version
+if errorlevel 9009 if not errorlevel 9010 goto notfoundjavac
+if not exist build.sbt goto notfoundbuildsbt
+set SCRIPT_DIR=%~dp0
+if exist %HOMEDRIVE%%HOMEPATH%\.ivy2\local\com.sciabarra\nul goto :corebuilt
+java -Xms128m -Xmx512m -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384M -Dsbt.boot.directory=project\boot -jar "%SCRIPT_DIR%bin\sbt-launch.jar" core/publish-local
+:corebuilt
+java -Xms128m -Xmx512m -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384M -Dsbt.boot.directory=project\boot -jar "%SCRIPT_DIR%bin\sbt-launch.jar" %*
+goto end
+:notfoundjava
+echo java not found. Please install JDK and set JAVA_HOME environment variable. 
+goto pause
+:notfoundjavac
+echo javac not found. Please install JDK and set JAVA_HOME environment variable. 
+goto pause
+:notfoundbuildsbt
+echo To start, copy build.sbt.sbt to build.sbt and EDIT IT.
+echo Reading the README.md and the documentation does not hurt, too. 
+:pause
+pause
+:end
+cls
--- agilesites.sh
+++ agilesites.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+if test -n "$JAVA_HOME"
+then PATH="$JAVA_HOME/bin:$PATH"
+fi
+if ! java -version
+then echo java not found. Please install JDK and set JAVA_HOME ; exit
+fi
+if ! javac -version
+then echo javac not found. Please install JDK and set JAVA_HOME ; exit
+fi
 if ! test -f build.sbt
 then 
  echo "To start, copy build.sbt.sbt to build.sbt and EDIT IT."
- echo "Reading the README.md and the book does not hurt, too." 
+ echo "Reading the README.md and the documentation does not hurt, too." 
 else 
-  if ! test -e project/core.built
+  if ! test -d $HOME/.ivy2/local/com.sciabarra
   then java -Xms256m -Xmx512m -Xss1m -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384m -Dsbt.boot.directory=project/boot/ -jar bin/sbt-launch.jar core/publish-local
-  touch project/core.built
   fi
   java -Xms256m -Xmx512m -Xss1m -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384m -Dsbt.boot.directory=project/boot/ -jar bin/sbt-launch.jar "$@"
 fi
