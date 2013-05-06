6da2d4aac7994d39a2abc76f0970dd28c492616e
Michele Sciabarra <michele@sciabarra.com>
2013-05-06 07:34:10 +0200

    fixed ci host

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 umask 0
 cp build.sbt.ci build.sbt
 echo Hello
-curl "http://localhost:8080/cs/HelloCS"
+curl "http://regulus.sciabarra.com/cs/HelloCS"
 echo Setup
 sh agilesites.sh "wcs-setup-online silent"
 echo Importing
@@ -12,4 +12,4 @@ sh agilesites.sh "app/clean"
 sh agilesites.sh "api/clean"
 sh agilesites.sh "wcs-deploy"
 echo Testing
-curl -o report.xml "http://localhost:8080/cs/ContentServer?xml=1&cs.contenttype=text/xml&pagename=Demo/DmTester"
+curl -o ${WORKSPACE:-.}/report.xml "http://regulus.sciabarra.com/cs/ContentServer?xml=1&cs.contenttype=text/xml&pagename=Demo/DmTester"
