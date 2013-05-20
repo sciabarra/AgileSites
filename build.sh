6208200df37dcb6fc2d55190081a647bddb2c688
Michele Sciabarra <michele@sciabarra.com>
2013-05-20 21:45:07 +0200

    updated javadoc restricting only certain files

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 H=../0.5
-javadoc -sourcepath "$H/core/src/main/java;$H/api/src/main/java" -subpackages "wcs" -d javadoc
 cp $H/*.md _includes
 cp -rvf $H/doc/img .
 for j in . tutorial reference ; do 
@@ -16,3 +15,8 @@ echo "---" ;\
 cat $i ) | sed -e 's!\.md!\.html!g' | perl -pe 's!\.?\./img/!/img/!g'  >$b
 echo $b
 done ; done
+# javadocs
+#javadoc @javadoc.files -subpackages "wcs" -d javadoc
+#javadoc -sourcepath "$H/core/src/main/java;$H/api/src/main/java" -subpackages "wcs" -d javadoc
+javadoc -subpackages "wcs" -d javadoc $(grep -v '#' javadoc.files) 
+
