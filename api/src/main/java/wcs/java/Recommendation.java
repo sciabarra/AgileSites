601cf5fdf21699f4ac7db86b12828a037e4bb9b0
Michele Sciabarra <michele@sciabarra.com>
2013-10-06 19:24:26 +0200

    reorg build and Populate

---
+++ api/src/main/java/wcs/java/Recommendation.java
@@ -0,0 +1,75 @@
+package wcs.java;
+
+import static wcs.core.Common.tmp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import wcs.core.Arg;
+import wcs.core.Id;
+import wcs.core.Log;
+import wcs.core.tag.CommercecontextTag;
+import COM.FutureTense.Interfaces.ICS;
+
+/**
+ * A decorator to add recommandation handling functions
+ * 
+ * @author msciab
+ * 
+ */
+public class Recommendation {
+
+	private static Log log = Log.getLog(Recommendation.class);
+
+	Env e;
+	ICS i;
+
+	String name = null;
+	Long id = null;
+
+	Arg[] args;
+
+	public Recommendation(Env env, String name, Arg[] args) {
+		e = env;
+		i = e.ics;
+		this.name = name;
+		this.args = args;
+	}
+
+	public Recommendation(Env env, Long id, Arg[] args) {
+		e = env;
+		i = e.ics;
+		this.id = id;
+		this.args = args;
+	}
+
+	/**
+	 * Return recommendation childrens as list of ID
+	 */
+	public List<Id> getChildren() {
+
+		CommercecontextTag.Getrecommendations rec = CommercecontextTag
+				.getrecommendations();
+
+		String output = tmp();
+		rec.listvarname(output);
+
+		if (id != null)
+			rec.collectionid(id.toString());
+		else
+			rec.collection(name);
+
+		rec.set(args).run(i);
+
+		log.debug("found " + e.GetList(output));
+
+		List<Id> result = new ArrayList<Id>();
+		if (e.isList(output))
+			for (int pos : e.getRange(output))
+				result.add(new Id(e.getString(output, pos, "assettype"), e
+						.getLong(output, pos, "assetid")));
+
+		return result;
+	}
+
+}
